49284fdd98b51e3af8e560490be61ec2abbaecf3
CosmicAsad <asy_7@hotmail.co.uk>
2021-02-11 18:43:18 +0000

    Completed user authentication pages

--- app.py
+++ app.py
@@ -45,7 +45,48 @@ def register():
         # Put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if username exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                # Invalid password
+                flash("Username and/or Password is incorrect")
+                return redirect(url_for("login"))
+
+        else:
+            # Username doesn't exist
+            flash("Username and/or Password is incorrect")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Retrieve username from MongoDB
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
